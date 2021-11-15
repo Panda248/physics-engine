d45cd607fd07713c2e70063c8515b593e928aed2
Panda248 <40081083+Panda248@users.noreply.github.com>
2021-11-15 16:03:03 -0500

    Update Collider.java

--- src/physics/primitives/java/Collider.java
+++ src/physics/primitives/java/Collider.java
@@ -8,47 +8,79 @@ public class Collider {
     public float height;
     public float x;
     public float y;
-
-    public float getWidth() {
+    public float getWidth()
+    {
         return this.width;
     }
-
-    public float getHeight() {
+    public float getHeight()
+    {
         return this.height;
     }
-
-    public float getX() {
+    public float getX()
+    {
         return this.x;
     }
-
-    public float getY() {
+    public float getY()
+    {
         return this.y;
     }
-
-    public void setX(int amount) {
+    public void setX(int amount)
+    {
         this.x = amount;
     }
-
-    public void setY(int amount) {
+    public void setY(int amount)
+    {
         this.y = amount;
     }
-
-    public void update(float x, float y, float width, float height) {
+    public void update(float x, float y, float width, float height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
-
-    public boolean isColliding(Collider collider) {
-        return isCollidingTop(collider) && isCollidingSide(collider);
+    public boolean isColliding(Collider collider)
+    {
+        return ((this.x < collider.x + collider.width && this.x + this.width > collider.x) &&(this.y < collider.y + collider.height && this.y + this.height > collider.y));
     }
-
-    public boolean isCollidingTop(Collider collider) {
-        return (this.y - this.height >= collider.y && this.y - this.height <= collider.y + collider.height) || (this.y >= collider.y && this.y <= collider.y + collider.getHeight());
+    public boolean isCollidingTop(Collider collider)
+    {
+        return this.y < collider.y + collider.height && (this.x < collider.x + collider.width && this.x + this.width > collider.x);
     }
+    public boolean isCollidingBottom(Collider collider)
+    {
+        return(this.y + height > collider.y) && (this.x < collider.x + collider.width && this.x + this.width > collider.x);
+    }
+    public boolean isCollidingLeft(Collider collider)
+    {
+        return this.x + width > collider.x && (this.y < collider.y + collider.height && this.y + this.height > collider.y);
+    }
+    public boolean isCollidingRight(Collider collider)
+    {
+        return this.x < collider.x + collider.width && (this.y < collider.y + collider.height && this.y + this.height > collider.y);
+    }
+    public char getDirection(Box box)
+    {
+        if(box.getX() < this.x)
+        {
+            return 'r';
+        }
+        else if(box.getX() > this.x)
+        {
+            return 'l';
+        }
+        else if(box.getY() < this.y)
+        {
+            return 'u';
+        }
+        else if(box.getY() > this.y)
+        {
+            return 'd';
+        }
+        else
+        {
+            return 'n';
+        }
 
-    public boolean isCollidingSide(Collider collider) {
-        return (this.x >= collider.x && this.x <= collider.x + collider.getWidth()) || (this.x + this.height >= collider.x && this.x + this.width <= collider.x + collider.getWidth());
     }
 }
+
